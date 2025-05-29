6a0e7a3497573c023edfb79f269be197d38f7e07
eupeu ;) <peutroleo@gmail.com>
2025-05-29 20:36:47 -0300

    add: teams profile service

---
+++ app/services/teams/profile.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from app.services.base import HLTVBase
+from app.utils.utils import extract_from_url
+
+from app.utils.xpath import Teams
+
+
+print(Teams.TeamProfile.LOGO_URL)
