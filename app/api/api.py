b5df48162194888683246c2c583a56ddb4e55929
Pedro Oliveira <peutroleo@gmail.com>
2025-05-29 16:08:23 -0300

    add: router for team services

--- app/api/api.py
+++ app/api/api.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 
 from app.api.endpoints import players
 from app.api.endpoints import events
+from app.api.endpoints import teams
 
 api_router = APIRouter()
 
 api_router.include_router(players.router, prefix="/players", tags =["Players"])
 
 api_router.include_router(events.router, prefix="/events", tags= ["Events"]) 
+
+api_router.include_router(teams.router, prefix ="/teams", tags = ["Teams"])
